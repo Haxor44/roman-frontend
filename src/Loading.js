5eebb7447cb410cc9fb28a1d7627e6e62d8604d0
Haxor44 <evolmalek04@gmail.com>
2023-09-05 15:53:48 +0300

    final display!!!

--- src/Loading.js
+++ src/Loading.js
@@ -1,24 +1,16 @@
 import React,{useState, useEffect} from "react";
 import { usePromiseTracker } from "react-promise-tracker";
 import {ThreeDots} from 'react-loader-spinner';
+import {Box} from "@mui/material";
 
  function  Loading  () {
        const { promiseInProgress } = usePromiseTracker();
     
        return (
         promiseInProgress && 
-        <div
-      style={{
-        width: "100%",
-        height: "100",
-        display: "flex",
-        marginTop:250,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <ThreeDots type="ThreeDots" color="#7B68EE" height="100" width="100" />
-      </div>
+        <Box  flex={4}>
+          <ThreeDots type="ThreeDots" color="#7B68EE" height="100" width="100" />
+      </Box>
       );  
      }
 
